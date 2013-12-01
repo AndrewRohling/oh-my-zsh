1b833dd8da9a490a0236c7559008c24b468e75bb
Andrew Rohling <andrew@andrewsdomain.net>
2013-12-01 13:03:38 -0500

    init rev

---
+++ custom/settings.zsh
@@ -0,0 +1,31 @@
+ZSH_THEME='andrew'
+export PGUSER=andrewrohling
+export PGDATABASE=postgres
+export PGPASSWORD=asdf
+export PGHOST=127.0.0.1
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home
+export ANT_HOME=/Users/andrewrohling/dev/ant
+export JVM_ARGS='-Xmx1024m -XX:MaxPermSize=256m'
+
+export HISTIGNORE='ls:ls *:cd:cd *:pwd;exit:date:* --help:history*:&'
+
+#export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/share/npm/bin:/Users/andrewrohling/bin
+
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/share/npm/bin:$ANT_HOME/bin:$PATH
+
+export EDITOR="vim"
+
+export HISTIGNORE="ls:ls *:cd:cd *:pwd;exit:date:* --help:history*:&"
+
+source /Users/andrewrohling/.rvm/scripts/rvm
+
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
+export IRCNAME="Sno"
