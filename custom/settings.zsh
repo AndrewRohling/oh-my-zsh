dc784d881abbdd86554737d02ab472b81dd20e04
Andrew Rohling <andrew@andrewsdomain.net>
2013-12-24 16:49:41 -0500

    updates

--- custom/settings.zsh
+++ custom/settings.zsh
@@ -1,11 +1,9 @@
 ZSH_THEME='andrew'
-export PGUSER=andrewrohling
-export PGDATABASE=postgres
-export PGPASSWORD=asdf
-export PGHOST=127.0.0.1
+
+source $HOME/.psql.conf
 
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home
-export ANT_HOME=/Users/andrewrohling/dev/ant
+export ANT_HOME=$HOME/dev/ant
 export ANT_OPTS=-Dbuild.sysclasspath=ignore
 export JVM_ARGS='-Xmx1024m -XX:MaxPermSize=256m'
 
@@ -17,7 +15,7 @@ export EDITOR="vim"
 
 export HISTIGNORE="ls:ls *:cd:cd *:pwd;exit:date:* --help:history*:&"
 
-source /Users/andrewrohling/.rvm/scripts/rvm
+source $HOME/.rvm/scripts/rvm
 
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;31m'
@@ -26,5 +24,3 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;44;33m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
-
-export IRCNAME="Sno"
